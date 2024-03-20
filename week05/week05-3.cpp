89005e9ffd8b7582106cd2cfdff6ab32ee7032dc
yw0522 <12750475@me.mcu.edu.tw>
2024-03-20 12:01:54 +0800

    week05

---
+++ week05/week05-3.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    for(int i=0;i<9;i++)///0-8
+    {
+        printf("i是%d ",i);
+    }
+    printf("\n");
+    for(int i=1;i<=9;i++)///1-9
+    {
+        printf("i是%d ",i);
+    }
+    printf("\n");
+    for(int i=9-1;i>=0;i--)///倒過來 8-0
+    {
+        printf("i是%d ",i);
+    }
+}
